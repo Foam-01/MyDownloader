f7109f6ece09600fdba57258c619bc0460b6030a
Foam-01 <ddotdka4@gmail.com>
2025-11-05 14:57:33 +0700

    Trigger new deploy to fix 502

--- app.py
+++ app.py
@@ -70,3 +70,4 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     print(f"เซิร์ฟเวอร์ (v4 Proxy) กำลังทำงานที่ http://localhost:{port}")
     app.run(host='0.0.0.0', port=port, debug=False)
+    # Trigger deploy v4.1
